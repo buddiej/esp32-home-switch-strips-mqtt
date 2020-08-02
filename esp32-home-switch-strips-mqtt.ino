5d713826d31170731634fb0364be86f052b31b24
buddiej <67640063+buddiej@users.noreply.github.com>
2020-08-02 22:48:20 +0200

    Update esp32-home-switch-strips-mqtt.ino
    Added OTP

--- esp32-home-switch-strips-mqtt.ino
+++ esp32-home-switch-strips-mqtt.ino
@@ -2,7 +2,7 @@
 #include <PubSubClient.h>
 #include <Wire.h>
 #include <ArduinoJson.h>
-
+#include <ArduinoOTA.h>
 
 #include "FastLED.h" 
 #include "arduinoFFT.h"
@@ -123,7 +123,55 @@ static void mqttconnect(void);
                               Serial.print("\t"); \
                               Serial.print("payload: "); \
                               Serial.println(PayloadString);
-                              
+
+/**************************************************************************************************
+Function: ArduinoOta_Init()
+Argument: void
+return: void
+**************************************************************************************************/
+void ArduinoOta_Init(void)
+{
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  ArduinoOTA.setHostname("esp32-home-switch-strips-mqtt");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+}                              
 /*************************************************************************************************/
 /**************************************************************************************************
 Function: setup()
@@ -148,6 +196,7 @@ void setup()
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) 
@@ -161,6 +210,9 @@ void setup()
       ESP.restart();
     }
   }
+
+  ArduinoOta_Init();
+  
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
@@ -200,6 +252,8 @@ return: void
 /*************************************************************************************************/
 void loop() 
 {
+
+  ArduinoOTA.handle();
   
   /* if client was disconnected then try to reconnect again */
   if (!client.connected()) {
